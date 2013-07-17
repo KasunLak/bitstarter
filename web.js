86887ea651f499a75a0c86d705159a2094679c23
KasunLak <kasun.lakpriya86@gmail.com>
2013-07-17 15:41:44 +0530

    Modified web.js

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var fs      = require('fs');
 
 var app = express.createServer(express.logger());
 
+var readIndex = function(){
+    var content = fs.readFileSync('index.html', 'utf8');
+    var buf = new Buffer(content, 'utf8');
+    return buf.toString('utf8');
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!'); 
+  response.send(readIndex()); 
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
